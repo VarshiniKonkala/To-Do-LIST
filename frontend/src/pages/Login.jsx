97c2ec14514f5c8571d329d52f2b29cdb8f9dbb1
VarshiniKonkala <150248333+VarshiniKonkala@users.noreply.github.com>
2024-08-25 23:51:46 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:5000/api/users/login", {
+    const response = await fetch("https://to-do-list-an7h.onrender.com/api/users/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
